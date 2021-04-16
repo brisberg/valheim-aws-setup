807db681855725c2c7655fb51b448c1b24b78dfc
Brandon Risberg <brandon.risberg@gmail.com>
2021-04-15 21:31:55 -0700

    Adds comment for install aws cli

--- server-init.sh
+++ server-init.sh
@@ -9,4 +9,7 @@ curl -o docker-compose.yml https://gist.githubusercontent.com/robzhu/a127a6bce1e
 # Manually update the config values
 
 # Launch the server
-sudo docker-compose up
+sudo docker-compose 
+
+# Install AWS CLI
+# sudo apt install awscli
