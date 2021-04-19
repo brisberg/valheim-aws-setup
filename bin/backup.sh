5c943f33cc76cf14f8017301d210fdfa7d1b556a
brisberg <brandon.risberg@gmail.com>
2021-04-18 19:15:20 -0700

    Fixes tar zips to operate on the correct directory

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Archive Saves directory
-tar -czf Paradiso.tar.gz ~/valheim/saves --exclude=*.old
+tar -C ~/valheim -czf ~/Paradiso.tar.gz --exclude=*.old ./saves
 
 # Upload to S3
 aws s3 mv Paradiso.tar.gz s3://valheim-1-backups/Paradiso/Paradiso.tar.gz
--- bin/restore.sh
+++ bin/restore.sh
@@ -5,4 +5,4 @@ aws s3 cp s3://valheim-1-backups/Paradiso/Paradiso.tar.gz ~/Paradiso.tar.gz
 
 # Replace Saves Directory
 rm -rf ~/valheim/saves
-tar -xzf ~/Paradiso.tar.gz ~/valheim/saves
+tar -C ~/valheim -xzf ~/Paradiso.tar.gz
